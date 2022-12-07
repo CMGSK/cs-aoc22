bce8279130b7f5a4be24c7fde7eb353ce8c40e5b
Carlos <carlosmgov7k@gmail.com>
2022-12-07 15:01:34 +0100

    day 7 logic wrong pt1

--- day7/Program.cs
+++ day7/Program.cs
@@ -18,6 +18,17 @@
         this.parent = parent;
       }
 
+      public void setSize (int n){
+        this.size=n;
+      }
+      public int getSize()
+      {
+        return size;
+      }
+      public bool getIsDir()
+      {
+        return isDir;
+      }
       public string getName()
       {
         return name;
@@ -32,7 +43,7 @@
       }
     }
 
-    static int Part1 (string[] input)
+    static long Part1 (string[] input)
     {
       List<Node> Root = new List<Node>();
       Root.Add(new Node(true, 0, "/", "//"));
@@ -62,12 +73,12 @@
             }
           }
         }
-        else if (input[x].Contains("$ ls"))
+        else if (input[x].Contains("$ ls")) //ls indicates where were creating files 
         {
           x++;
-          while (!(input[x].Contains("$")) && x+1<input.Count())
+          while (!(input[x].Contains("$")) && x+1<input.Count()) // && to avoid out of bounds on last items
           {
-            if(!(input[x].Contains("dir")))
+            if(!(input[x].Contains("dir"))) // we ignore dir since we create them with cd commands, therefore only files are created after ls command
             {
               Root.Add(new Node(false, 
                     int.Parse(input[x].Substring(0, input[x].IndexOf(" "))), 
@@ -78,8 +89,9 @@
           }
         }
       }
+      // Sanity check
       foreach (var t in Root) t.printNode();
-      return 0;
+      return sizeCalc(Root);
     }
 
     static int Part2 (string[] input)
@@ -88,12 +100,37 @@
       return 0;
     }
 
-    static int sizeCalc (List<Node> Root)
+    static long sizeCalc (List<Node> Root)
     {
-
-
-
-      return 0;
+      for (int i=Root.Count()-1; i>=0; i--)
+      {
+          Console.WriteLine("here");
+        int size=0;
+        string last = Root.ElementAt(i).getParent();
+        bool refIndex = false;
+        while (!Root.ElementAt(i).getIsDir() && Root.ElementAt(i).getParent() == last)
+        {
+          size+=Root.ElementAt(i).getSize();
+          last = Root.ElementAt(i).getParent();
+          i--;
+          refIndex=true;
+        }
+        if (refIndex) i++;
+        for(int j=0; j<Root.Count(); j++)
+        {
+          if (Root.ElementAt(j).getName() == last) Root.ElementAt(j).setSize(size);
+        }
+      }
+      //now we sum the folders properly
+      long result=0;
+      for (int i=0; i<Root.Count(); i++)
+      {
+        if (Root.ElementAt(i).getIsDir() && Root.ElementAt(i).getSize() < 100000)
+        {
+          result += Root.ElementAt(i).getSize();
+        }
+      }
+      return result;
     }
 
     public static void Main (string[] args)
