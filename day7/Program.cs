5a3c4ac660ea59be958913a539fbd7307d832af1
Carlos <carlosmgov7k@gmail.com>
2022-12-10 12:25:52 +0100

    day 7 full!

--- day7/Program.cs
+++ day7/Program.cs
@@ -66,19 +66,6 @@
       }
     }
 
-    static int Part1 (string[] input)
-    {
-      Node root = getRoot(input);
-      setDirSize(root);
-      Console.WriteLine(root.ToString());
-      return getDirSize(root);
-    }
-
-    static int Part2 (string[] input)
-    {
-
-      return 0;
-    }
 
     static Node getRoot (string[] input)
     {
@@ -129,7 +116,6 @@
         }
       }
       dir.setSize(size);
-      Console.WriteLine(size);
       return size;
     }
 
@@ -147,10 +133,45 @@
       return size; 
     }
 
+    static int getDelete (Node dir, int toFree, int lowest)
+    {
+      int size = dir.getSize();
+      if (size < toFree) size=0;
+      else if (size < lowest && size >= toFree) lowest = size;
+      if (dir.getDirs().Any())
+      {
+        foreach (var y in dir.getDirs())
+        {
+          if (getDelete(y, toFree, lowest) < lowest && getDelete(y, toFree, lowest) >= toFree) lowest = getDelete(y, toFree, lowest);
+        }
+      }
+      return lowest; 
+    }
+
+/////////////////////////////////////////////////////////////////////////
+
+    static int Part1 (string[] input)
+    {
+      Node root = getRoot(input);
+      setDirSize(root);
+      // Console.WriteLine(root.ToString());
+      return getDirSize(root);
+    }
+
+    static int Part2 (string[] input)
+    {
+      Node root = getRoot(input);
+      setDirSize(root);
+      int toFree = 30000000 - (70000000 - root.getSize());
+      Console.WriteLine($"Space to free{toFree}");
+      return getDelete(root, toFree, root.getSize());
+    }
+
     public static void Main (string[] args)
     {
       string[] input = File.ReadAllLines("input.txt");
       Console.WriteLine(Part1(input));
+      Console.WriteLine(Part2(input));
     }
   }
 }
